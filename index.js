b754ce3b9bc998734f5381beb2dfcb03a0581050
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-02-06 10:09:55 +0200

    declare input element and use switch statement

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const celsiusEl = document.getElementById("celsius");
+const fahrenheitEl = document.getElementById("fahrenheit");
+const kelvinEl = document.getElementById("kelvin");
+
 function computeTemp(event) {
-  console.log(event.target.name);
+  //console.log(event.target.name);
+  //console.log(event.target.value);
+  const currentValue = +event.target.value;
+  //The plus sign is to ensure that we always have a number
+  switch (event.target.name) {
+    case "celsius":
+      kelvinEl.value = currentValue + 273.32;
+      fahrenheitEl.value = currentValue * 1.8 + 32;
+      break;
+    case "fahrenheit":
+      celsiusEl.value = (currentValue - 32) / 1.8;
+      kelvinEl.value = (currentValue - 32) / 1.8 + 273.32;
+      break;
+    case "kelvin":
+      celsiusEl.value = currentValue - 273.32;
+      fahrenheitEl.value((currentValue - 273.32) * 1.8 + 32);
+      break;
+    default:
+      break;
+  }
 }
